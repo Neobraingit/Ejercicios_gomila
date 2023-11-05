6a4b20f8be9451f2a508dbc26e01af16362812a1
Neobraingit <Maervca2@icloud.com>
2023-11-05 18:00:00 +0100

    100_ejercicios

---
+++ 01_Secuencia_de_variables.py
@@ -0,0 +1,7 @@
+'''
+Secuencia de asignación de variables y mostrarlas por consola
+'''
+var1, var2, var3 = 'perro', 'gato', 'pez'
+print (var1)
+print (var2)
+print (var3)
---
+++ 03_Serie_de_numeros.py
@@ -0,0 +1,3 @@
+for r in range (0, 11):
+    print (r)
+ 
