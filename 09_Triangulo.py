44a3b7739ecd8cdf4b433c7caa15d02b9d94036b
Neobraingit <Maervca2@icloud.com>
2023-11-05 18:27:42 +0100

    chatgpt

---
+++ 09_Triangulo.py
@@ -0,0 +1,6 @@
+# Calcular el área de un triángulo con base y altura ingresadas por el usuario.
+
+base = int(input('Introduce la base del triángulo: '))
+altura = int(input('Introduce la altura del triángulo: '))
+
+print ('El área de un triángulo es: {}'.format((base * altura)/ 2))
---
+++ 10_Circulo.py
@@ -0,0 +1,1 @@
+# Calcular el área de un círculo con el radio ingresado por el usuario
