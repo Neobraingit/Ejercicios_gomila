6cbba14cbc7f73112401dff57c784c921a804506
Neobraingit <Maervca2@icloud.com>
2023-11-05 12:01:04 +0100

    strings

---
+++ Nacimient_edad.py
@@ -0,0 +1,5 @@
+ano_nacimiento = int(input('Introduce tu año de nacimiento: '))
+ano_actual = int(input('Introduce tu año actual: '))
+edad = ano_actual - ano_nacimiento
+
+print ('Tu edad en el año {} es de {}'.format (ano_actual,edad))
