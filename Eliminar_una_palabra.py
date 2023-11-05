fae8897a04fa328fb1d10802863dde87815f0724
Neobraingit <Maervca2@icloud.com>
2023-11-05 11:33:43 +0100

    Segundo commit

--- Eliminar_una_palabra.py
+++ Eliminar_una_palabra.py
@@ -1,1 +1,7 @@
-print ('Me llamo Marcos')
+string = 'Me llamo Marcos Carmona García'
+print (string)
+palabra= input('Introduce la palabra a eliminar: ')
+indice = string.find(palabra)
+substring = string [:indice] + string [indice + len(palabra): ]
+print (substring)
+
