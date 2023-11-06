3f874499b66cc8b0f7c21830edb42c239c85c9fc
Neobraingit <Maervca2@icloud.com>
2023-11-06 21:10:33 +0100

    chatGpt

---
+++ 26_Area_cuadrado.py
@@ -0,0 +1,8 @@
+
+
+lado1 = float(input('Introduce el largo del lado en cm: '))
+lado2 = float(input('Introduce el largo del otro lado en cm: '))
+
+area = lado1 * lado2
+
+print ('El área del cuadrado son {} cm...'.format(area))
