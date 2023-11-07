4d5437669e17c0d76499d79e38a174533870d286
Neobraingit <Maervca2@icloud.com>
2023-11-07 17:55:52 +0100

    chatGpt

---
+++ 27_Area_trapecio.py
@@ -0,0 +1,9 @@
+# Calcular el área de un trapecio.
+
+base1 = float(input('Introduce el alto de la base uno: '))
+base2 = float(input('Introduce el alto de la base dos: '))
+altura = float(input('Introduce la altura: '))
+
+trapecio = (base1 + base2) * altura
+
+print ('El area del trapecio es: {}'.format(trapecio))
